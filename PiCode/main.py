89afaf906093a91daed59c8a28f021cc965d9166
Hongsworth <therchgaming@gmail.com>
2023-02-01 15:12:39 +0000

    read temperature and humidity

---
+++ PiCode/main.py
@@ -0,0 +1,40 @@
+import time
+import smbus2
+
+si7021_ADD = 0x40
+si7021_READ_TEMPERATURE = 0xE3
+si7021_READ_HUMIDITY = 0xE5
+
+bus = smbus2.SMBus(1)
+start = 1
+
+#Set up a write transaction that sends the command to measure temperature
+cmd_meas_temp = smbus2.i2c_msg.write(si7021_ADD,[si7021_READ_TEMPERATURE])
+cmd_meas_hum = smbus2.i2c_msg.write(si7021_ADD,[si7021_READ_HUMIDITY])
+#Set up a read transaction that reads two bytes of data
+read_result = smbus2.i2c_msg.read(si7021_ADD,2)
+
+
+while(start == 1):
+
+    #Execute the two transactions with a small delay between them
+    bus.i2c_rdwr(cmd_meas_temp)
+    time.sleep(0.1)
+    bus.i2c_rdwr(read_result)
+
+    #convert the result to an int
+    temperature = int.from_bytes(read_result.buf[0]+read_result.buf[1],'big')
+    celcius = ((175.72 * temperature)/65536) - 46.85
+    print("temperature", celcius)
+    time.sleep(0.5)
+
+    bus.i2c_rdwr(cmd_meas_hum)
+    time.sleep(0.1)
+    bus.i2c_rdwr(read_result)
+
+
+    humidity = int.from_bytes(read_result.buf[0]+read_result.buf[1],'big')
+    percent_hum = ((125*humidity)/65536)-6
+
+    print("relative humidity:", percent_hum)
+    time.sleep(0.5)
--- PiCode/test.py
+++
@@ -1,5 +0,0 @@
-import numpy
-
-
-%This is just a small testing ground for the raspberry Pi input and output pins
-%nothing major.
