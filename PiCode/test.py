3bfc747866fe86919aaac0ee3bbb46d3f5ea11c6
Angelo A <angelo.2001.05@gmail.com>
2023-02-03 16:46:49 +0000

    Testing

--- PiCode/test.py
+++ PiCode/test.py
@@ -15,11 +15,16 @@ cmd_meas_temp = smbus2.i2c_msg.write(si7021_ADD,[si7021_READ_TEMPERATURE])
 #Set up a read transaction that reads two bytes of data
 read_result = smbus2.i2c_msg.read(si7021_ADD,2)
 
+
+
 #Execute the two transactions with a small delay between them
 bus.i2c_rdwr(cmd_meas_temp)
 time.sleep(0.1)
 bus.i2c_rdwr(read_result)
 
+
+
 #convert the result to an int
 temperature = int.from_bytes(read_result.buf[0]+read_result.buf[1],'big')
-print(temperature)
+celcius = ((175.72 * temperature)/65536) - 46.85
+print("Temperature: %d", celcius)
