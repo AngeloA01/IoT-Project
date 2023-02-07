938e15dff067e3b7a2f61046d933383997f1d61a
Ziyad Ansari <zaa120@ic.ac.uk>
2023-02-07 18:16:10 +0000

    initialized firebase on webapp

---
+++ Web App/initialize.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAz1R9Y4rgIUDR4TINqXvG2BbjZGzlWbXs",
+  authDomain: "embedded-lab-2-part-2.firebaseapp.com",
+  databaseURL: "https://embedded-lab-2-part-2-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "embedded-lab-2-part-2",
+  storageBucket: "embedded-lab-2-part-2.appspot.com",
+  messagingSenderId: "164614426559",
+  appId: "1:164614426559:web:cf7044cee5ca58327adb0a",
+  measurementId: "G-FNKN7NGVYY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
---
+++ Web App/initializeapp.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+const app = initializeApp(firebaseConfig);
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAz1R9Y4rgIUDR4TINqXvG2BbjZGzlWbXs",
+  authDomain: "embedded-lab-2-part-2.firebaseapp.com",
+  databaseURL: "https://embedded-lab-2-part-2-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "embedded-lab-2-part-2",
+  storageBucket: "embedded-lab-2-part-2.appspot.com",
+  messagingSenderId: "164614426559",
+  appId: "1:164614426559:web:cf7044cee5ca58327adb0a",
+  measurementId: "G-FNKN7NGVYY"
+};
