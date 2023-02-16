b0359ac0faeac0e18c04c8b7c8a1ef41d45af873
Ziyad Ansari <zaa120@ic.ac.uk>
2023-02-16 17:37:06 +0000

    fixed

--- Web App/get_advice.py
+++ Web App/get_advice.py
@@ -1,12 +1,16 @@
-import read_humidity1
+import read_all_data
 # curr_CO2 = read_C02.get_C02()
 # curr_pressure = read_pressure.get_pressure()
 # curr_temp = read_temp_data.get_temperature()
 # curr_tvoc = read_TVOC.get_TVOC()
-curr_humidity = read_humidity1.get_humidity()
+curr_humidity = read_all_data.get_humidity()
+curr_temp = read_all_data.get_temperature()
+curr_tvoc = read_all_data.get_TVOC()
+curr_pressure = read_all_data.get_pressure()
+curr_CO2 = read_all_data.get_C02()
 def getadvice():
     advices = ["It's hot outside, please drink lots of water", "Try drinking water"]
-    if curr_humidity>10:
+    if curr_temp>10:
         return advices[0]
     else:
         return advices[1]
