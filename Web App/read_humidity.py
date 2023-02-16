81fa7c16665e8807b6f12355bf081d352eb780cc
rana2302 <67081360+rana2302@users.noreply.github.com>
2023-02-16 13:08:52 +0000

    15 feb

--- Web App/read_humidity.py
+++ Web App/read_humidity.py
@@ -3,17 +3,16 @@ import requests
 db = "https://embedded-lab-2-part-2-default-rtdb.europe-west1.firebasedatabase.app/"
 def get_humidity():
         
-        response = requests.get(db+"/humidity/.json")
-        humidity = response.json()
-        hum_list = list(humidity.values())
-        print("Initial humidity: ",humidity)
+        response = requests.get(db+"/Humidity.json")
+        Humidity = response.json()
+        hum_list = list(Humidity.values())
         k=0
         sum=0
         while (k<len(hum_list)):
             p = hum_list[k]
-            p1 = p["Humidity: "]
+            p1 = p["Humidity"]
             sum = sum + p1
             k+=1
         avg = sum/k
         return round(avg, 1)
-
+print(get_humidity())
